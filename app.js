affdd2a5fe742268dca5faa98642d512ee1bafeb
Hasen Ali <ali786.hasen@gmail.com>
2024-12-01 09:51:58 +0530

    Added index file, style.css, app.js and image

---
+++ app.js
@@ -0,0 +1,26 @@
+let i=0;
+let n=1;
+let cArray=["red","yellow","green"];
+function trafficLight(){
+    let l=document.getElementsByClassName('light'); //[3]
+    for(let j=0; j<l.length; j++){
+        l[j].style.background="black";
+        l[j].innerHTML="";
+    }
+    l[i].style.background=cArray[i];
+    if(i<l.length){
+        if(n<=3){
+            l[i].innerHTML=n;
+            n++;
+        }
+        if(n==4){
+            i++;
+            n=1;
+            if(i==3){
+                i=0;
+            }
+        }
+    }
+       
+}
+setInterval(trafficLight,1000);
